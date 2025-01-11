ac1d49d857f7af94192cefd196000f449e877470
Omegadefinitiv <118939168+Omegadefinitiv@users.noreply.github.com>
2025-01-10 19:32:53 -0600

    sintaxis

--- numero00.java
+++ numero00.java
@@ -1,5 +1,11 @@
 public class numero00 {
+    //https://www.java.com/es/
     public static void main(String[] args) {
-        
+        int entero = 1;
+        String texto = "Hola";
+        boolean TF = true;
+        float flotante = 2.2f;
+        double decimal = 2.555;
+        System.out.println("Hola java!");
     }
 }
